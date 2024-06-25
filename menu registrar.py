686cef25a72348cb2b70a67e4e8004d67c3d2d4c
amarowexd999 <amar.escobar@duocuc.cl>
2024-06-25 11:54:57 -0400

    primera parte registro de ventas

---
+++ menu registrar.py
@@ -0,0 +1,18 @@
+
+while True:
+    print("****** MENU PRINCIPAL *******")
+    print("1)	Registrar pedido" )   
+    print("2)	Listar los todos los pedidos")  
+    print("3)	Imprimir hoja de ruta")
+    print("4)	Salir del programa")
+    op= input("seleccione opcion: ")
+
+    if op=="1":
+            print("entrando opcion 1")
+    elif op=="2":
+         print("entrando opcion 2")
+    elif op=="3":
+        print("entrando opcion 3")
+    elif op=="4":
+        print("saliendo del programa")
+        break
